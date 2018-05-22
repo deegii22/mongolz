d84435a039430faef5b03f2405a1712a008c3ba1
Enkhtulga <ts.enkhtulga@gmail.com>
2018-05-22 17:53:37 -0500

    fix bugs

--- MongolzRest/src/main/java/com/mongolz/amqp/AccountServiceImpl.java
+++ MongolzRest/src/main/java/com/mongolz/amqp/AccountServiceImpl.java
@@ -6,8 +6,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 public class AccountServiceImpl implements AccountService {
     public void publish(RabbitTemplate rabbitTemplate) {
 
-        Account a1 = new Account("1234");
-        Account a2 = new Account("5678");
+        Account a1 = new Account("1234",1000);
+        Account a2 = new Account("5678",1000);
 
         rabbitTemplate.convertAndSend(a1);
         rabbitTemplate.convertAndSend(a2);

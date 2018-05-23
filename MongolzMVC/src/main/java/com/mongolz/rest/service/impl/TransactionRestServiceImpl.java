4d19f8d1559c980b0d3a0dcdf0a72fca063b3e0b
Enkhee <3nkh33@gmail.com>
2018-05-23 12:31:20 -0500

    transaction

--- MongolzMVC/src/main/java/com/mongolz/rest/service/impl/TransactionRestServiceImpl.java
+++ MongolzMVC/src/main/java/com/mongolz/rest/service/impl/TransactionRestServiceImpl.java
@@ -4,7 +4,9 @@ import com.mongolz.domain.Transaction;
 import com.mongolz.rest.RestHttpHeader;
 import com.mongolz.rest.service.TransactionRestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 import java.util.List;
@@ -15,13 +17,17 @@ public class TransactionRestServiceImpl implements TransactionRestService {
     @Autowired
     RestHttpHeader restHelper;
 
-    String baseUrl = "http://localhost:8080/accounts";
+    String baseUrl = "http://localhost:8080/transactions";
     String baseUrlExtended = baseUrl + "/";
 
-    public void doTransaction(Transaction transaction){
+    public void doTransaction(Transaction transaction) {
+        RestTemplate restTemplate = restHelper.getRestTemplate();
+        HttpEntity<Transaction> httpEntity = new HttpEntity<>(transaction, restHelper.getHttpHeaders());
+        restTemplate.postForObject(baseUrlExtended + "txn/", httpEntity, Transaction.class);
 
     }
-    public List<Transaction> findByAccountAndDate(Long accountNo, Date startTxnDate, Date endTxnDate){
+
+    public List<Transaction> findByAccountAndDate(Long accountNo, Date startTxnDate, Date endTxnDate) {
         return null;
     }
 }
